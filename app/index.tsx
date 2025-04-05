81ec875770737f0588f9b28a5dc01fb7f43a3675
Ahmet Suat CAN <suatahmet@hotmail.com>
2025-04-06 02:49:37 +0300

    Deleted template code and created main project structure

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "gray",
+        alignItems: "center",
+      }}
+    >
+      <Text>Starting the Project!</Text>
+    </View>
+  );
+}
